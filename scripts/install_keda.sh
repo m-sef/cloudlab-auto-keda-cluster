2db51f40664ce972ed7eafe9110f442f47b635ca
m-sef <seth.l.moore0506@gmail.com>
2026-06-02 15:58:07 -0400

    Add helm repo setup and reference comment to install_keda.sh

--- scripts/install_keda.sh
+++ scripts/install_keda.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+# Brief: https://keda.sh/docs/2.19/deploy/
 
+# To deploy KEDA using Helm, first add the official KEDA Helm repository:
+helm repo add kedacore https://kedacore.github.io/charts
+helm repo update
+
+# Install keda by running:
 helm install keda kedacore/keda --namespace keda --create-namespace
 
 exit 0
