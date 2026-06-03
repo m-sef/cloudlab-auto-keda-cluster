2352ac7f48c949befb1813f50684d53a104dbc36
m-sef <seth.l.moore0506@gmail.com>
2026-06-03 08:58:48 -0400

    Added sudo to join_as_worker.sh ssh command

--- scripts/join_as_worker.sh
+++ scripts/join_as_worker.sh
@@ -4,7 +4,7 @@ MASTER=master
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if ! "$SCRIPT_DIR/is_master.sh"; then
-	JOIN_CMD=$(ssh -o StrictHostKeyChecking=no $MASTER "sudo kubeadm token create --print-join-command 2>/dev/null")
+	JOIN_CMD=$(sudo ssh -o StrictHostKeyChecking=no $MASTER "sudo kubeadm token create --print-join-command 2>/dev/null")
 
     until sudo $JOIN_CMD; do
 		sleep 5
