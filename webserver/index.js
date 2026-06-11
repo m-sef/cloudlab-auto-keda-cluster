101fb81b434b39aceb1b1a8f437202100da4675c
m-sef <seth.l.moore0506@gmail.com>
2026-06-11 09:27:31 -0400

    Removed nginx from webserver

--- webserver/index.js
+++ webserver/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app     = express();
 const port    = 8080;
 
-app.get("/", (request, response) => {
+let http_requests_total = 0;
+
+app.get('/metrics', (request, response) => {
+    response.status(200).send(`# TYPE http_requests_total counter\nhttp_requests_total ${http_requests_total}`);
+})
+
+app.get('/', (request, response) => {
+    http_requests_total += 1;
+
     const time_ms    = parseFloat(request.query.burn);
     const time_start = Date.now();
     const time_end   = time_start + time_ms;
@@ -16,7 +24,7 @@ app.get("/", (request, response) => {
 
     console.log(`burn=${time_ms}, ${x}`);
 
-    response.sendStatus(200);
+    response.status(200).send('ok');
 })
 
 app.listen(port, () => {
