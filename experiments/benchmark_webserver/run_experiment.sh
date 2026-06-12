428a06cc96d6f2d5765fb6e521f3b3a5c053322b
Seth Moore <seth.l.moore0506@gmail.com>
2026-06-12 15:32:34 -0400

    Modified run_experiment.sh

--- experiments/benchmark_webserver/run_experiment.sh
+++ experiments/benchmark_webserver/run_experiment.sh
@@ -3,10 +3,6 @@
 #            Seth Moore (slmoore@hamilton.edu)
 
 # --- CONFIGURATION (Hardcoded for simplicity) ---
-TOTAL_RUNS=1 # How many times to rerun the whole experiment
-TARGET_URL="http://$(kubectl get svc webserver-service -n webserver -o jsonpath='{.spec.clusterIP}')"
-
-BENCHMARK_NAME=$1
 
 # Helper function to run Vegeta load
 run_rate()
@@ -14,57 +10,69 @@ run_rate()
   local rate=$1
   local duration=$2
   local log_file=$3
-  echo "GET ${TARGET_URL}:8080/?burn=35" | \
+  echo "GET ${TARGET_URL}:8080/?burn=30" | \
       vegeta attack -rate="${rate}/s" -duration="${duration}" | \
       vegeta encode -to=csv -output="${log_file}"
 }
 
-run_experiment()
+run_experiments()
 {
+    NAME=$1
+    YAML=$2
+    RUNS=$3
+
+    kubectl apply -f $YAML
+    
+    sleep 60
+
+    TARGET_URL="http://$(kubectl get svc webserver-service -n webserver -o jsonpath='{.spec.clusterIP}')"
+
     # --- MAIN LOOP FOR RERUNS ---
-    for run_id in $(seq 1 ${TOTAL_RUNS}); do
-    echo "=========================================="
-    echo " STARTING EXPERIMENT RUN: ${run_id} @ ${TARGET_URL}"
-    echo "=========================================="
-    
-    # 1. Create a clean folder for this specific run's results
-    FOLDER_NAME="results/${BENCHMARK_NAME}/experiment_run_${run_id}"
-    mkdir -p "${FOLDER_NAME}"
+    for run_id in $(seq 1 ${RUNS}); do
+        echo "=========================================="
+        echo " STARTING EXPERIMENT RUN: ${run_id} @ ${TARGET_URL}"
+        echo "=========================================="
+        
+        # 1. Create a clean folder for this specific run's results
+        FOLDER_NAME="results/${NAME}/experiment_run_${run_id}"
+        mkdir -p "${FOLDER_NAME}"
 
-    # 2. Start HPA monitoring in the background and log to watch.log
-    echo "NAME   REFERENCE        TARGETS        MINPODS   MAXPODS   REPLICAS   AGE" > "${FOLDER_NAME}/replicas.log"
-    
-    # This loop runs silently in the background ($! grabs its process ID)
-    while true; do
-        kubectl get hpa -n webserver | tail -n 1 >> "${FOLDER_NAME}/replicas.log" 2>/dev/null
-        sleep 1
-    done &
-    WATCHER_PID=$!
+        # 2. Start HPA monitoring in the background and log to watch.log
+        echo "NAME   REFERENCE        TARGETS        MINPODS   MAXPODS   REPLICAS   AGE" > "${FOLDER_NAME}/replicas.log"
+        
+        # This loop runs silently in the background ($! grabs its process ID)
+        while true; do
+            kubectl get hpa -n webserver | tail -n 1 >> "${FOLDER_NAME}/replicas.log" 2>/dev/null
+            sleep 1
+        done &
+        WATCHER_PID=$!
 
-    # 3. Execute the Traffic Ramp
-    echo "--- Baseline: 10 req/s (1 minute) ---"
-    run_rate 10 "1m" "${FOLDER_NAME}/vegeta1.log"
+        # 3. Execute the Traffic Ramp
+        echo "--- Baseline: 10 req/s (1 minute) ---"
+        run_rate 10 "1m" "${FOLDER_NAME}/vegeta1.log"
 
-    echo "--- Spike: 60 req/s (3 minutes) ---"
-    run_rate 60 "3m" "${FOLDER_NAME}/vegeta2.log"
+        echo "--- Spike: 60 req/s (3 minutes) ---"
+        run_rate 60 "3m" "${FOLDER_NAME}/vegeta2.log"
 
-    echo "--- Cool down: 10 req/s (1 minute) ---"
-    run_rate 10 "1m" "${FOLDER_NAME}/vegeta3.log"
+        echo "--- Cool down: 10 req/s (1 minute) ---"
+        run_rate 10 "1m" "${FOLDER_NAME}/vegeta3.log"
 
-    # 4. Ramp finished. Hold and watch for an extra 10 minutes
-    echo "--- Traffic ramp finished. Monitoring HPA for an extra 5 minutes... ---"
-    sleep 5m
+        # 4. Ramp finished. Hold and watch for an extra 5 minutes
+        echo "--- Traffic ramp finished. Monitoring HPA for an extra 5 minutes... ---"
+        sleep 5m
 
-    # 5. Stop the background HPA watcher
-    kill ${WATCHER_PID}
-    
-    echo "RUN ${run_id} COMPLETE! Output saved to folder: ${FOLDER_NAME}"
-    echo "------------------------------------------"
+        # 5. Stop the background HPA watcher
+        kill ${WATCHER_PID}
+        
+        echo "RUN ${run_id} COMPLETE! Output saved to folder: ${FOLDER_NAME}"
+        echo "------------------------------------------"
     done
 
     echo "ALL RUNS COMPLETED SUCCESSFULLY."
+
+    kubectl delete -f $YAML
 }
 
-run_experiment
+run_experiments keda_prometheus yaml/webserver_keda_prometheus.yaml 2
 
 exit 0
